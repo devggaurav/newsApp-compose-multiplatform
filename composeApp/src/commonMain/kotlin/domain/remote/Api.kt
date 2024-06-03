25f5935011a0658314dbbef63aafa67ef433c916
gc <dev.gauravchauhan@gmail.com>
2024-06-03 12:14:30 +0530

    adding dependencies

---
+++ composeApp/src/commonMain/kotlin/domain/remote/Api.kt
@@ -0,0 +1,17 @@
+package domain.remote
+
+
+//
+// Created by Code For Android on 03/06/24.
+// Copyright (c) 2024 CFA. All rights reserved.
+//
+
+class Api {
+
+
+
+    companion object{
+        val baseUrl = "https://newsapi.org/v2/"
+        val apiKey = "f1ced744789e4137b0485b3e51749b48"
+    }
+}
